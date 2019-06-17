2dc82a2bdd2f85597156d7b4acddb09bd35ae941
Hady Eslam <abdoaslam000@gmail.com>
2019-06-17 09:22:19 +0200

    The Commit Of 17/6/2019

---
+++ Histogram Equalization - Team 9/ImageProccessingProject/ImageProccessingProject/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ImageProccessingProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(691, 542);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "H . E";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 233);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 271);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(421, 256);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(468, 32);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(426, 233);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(468, 271);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(426, 256);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 596);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Histogram Equalization - Team 9/ImageProccessingProject/ImageProccessingProject/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageProccessingProject
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap OriginalImage;
+        int w, h, r, g, b, x = 0, y = 0, count = 0;
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    OriginalImage = new Bitmap(openFileDialog.FileName);
+                    w = OriginalImage.Width;
+                    h = OriginalImage.Height;
+                }
+                catch
+                {
+                    MessageBox.Show("Can't open file.");
+                }
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = OriginalImage;
+
+                // Get Histogram
+                
+                Bitmap bmp = new Bitmap(OriginalImage);
+                int[] histogram_r = new int[256];
+                float max = 0;
+
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        int redValue = bmp.GetPixel(i, j).R;
+                        histogram_r[redValue]++;
+                        if (max < histogram_r[redValue])
+                            max = histogram_r[redValue];
+                    }
+                }
+
+                int histHeight = 232;
+                Bitmap img = new Bitmap(256, histHeight + 10);
+                using (Graphics g = Graphics.FromImage(img))
+                {
+                    for (int i = 0; i < histogram_r.Length; i++)
+                    {
+                        float pct = histogram_r[i] / max;   // What percentage of the max is this value?
+                        g.DrawLine(Pens.Black,
+                            new Point(i, img.Height - 5),
+                            new Point(i, img.Height - 5 - (int)(pct * histHeight))  // Use that percentage of the height
+                            );
+                    }
+                }
+                pictureBox2.Image = img;
+            }
+        }
+
+        static double Clamp(double val, double min, double max)
+        {
+            return Math.Min(Math.Max(val, min), max);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double blackPointPercent = 0.01;
+            double whitePointPercent = 0.03;
+            BitmapData srcData = OriginalImage.LockBits(new Rectangle(0, 0, OriginalImage.Width, OriginalImage.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            Bitmap destImage = new Bitmap(OriginalImage.Width, OriginalImage.Height);
+
+            BitmapData destData = destImage.LockBits(new Rectangle(0, 0, destImage.Width, destImage.Height),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            int stride = srcData.Stride;
+            IntPtr srcScan0 = srcData.Scan0;
+            IntPtr destScan0 = destData.Scan0;
+            var freq = new int[256];
+            unsafe
+            {
+                byte* src = (byte*)srcScan0;
+                for (int y = 0; y < OriginalImage.Height; ++y)
+                {
+                    for (int x = 0; x < OriginalImage.Width; ++x)
+                    {
+                        ++freq[src[y * stride + x * 4]];
+                    }
+                }
+
+                int numPixels = OriginalImage.Width * OriginalImage.Height;
+                int minI = 0;
+                var blackPixels = numPixels * blackPointPercent;
+                int accum = 0;
+
+                while (minI < 255)
+                {
+                    accum += freq[minI];
+                    if (accum > blackPixels) break;
+                    ++minI;
+                }
+
+                int maxI = 255;
+                var whitePixels = numPixels * whitePointPercent;
+                accum = 0;
+
+                while (maxI > 0)
+                {
+                    accum += freq[maxI];
+                    if (accum > whitePixels) break;
+                    --maxI;
+                }
+                double spread = 255d / (maxI - minI);
+                byte* dst = (byte*)destScan0;
+                for (int y = 0; y < OriginalImage.Height; ++y)
+                {
+                    for (int x = 0; x < OriginalImage.Width; ++x)
+                    {
+                        int i = y * stride + x * 4;
+
+                        byte val = (byte)Clamp(Math.Round((src[i] - minI) * spread), 0, 255);
+                        dst[i] = val;
+                        dst[i + 1] = val;
+                        dst[i + 2] = val;
+                        dst[i + 3] = 255;
+                    }
+                }
+                OriginalImage.UnlockBits(srcData);
+                destImage.UnlockBits(destData);
+                pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox3.Image = destImage;
+                Bitmap bmpb = new Bitmap(destImage);
+                int[] histogram_r = new int[256];
+                float max = 0;
+
+                for (int i = 0; i < bmpb.Width; i++)
+                {
+                    for (int j = 0; j < bmpb.Height; j++)
+                    {
+                        int redValue = bmpb.GetPixel(i, j).R;
+                        histogram_r[redValue]++;
+                        if (max < histogram_r[redValue])
+                            max = histogram_r[redValue];
+                    }
+                }
+
+                int histHeight = 232;
+                Bitmap imga = new Bitmap(256, histHeight + 10);
+                using (Graphics g = Graphics.FromImage(imga))
+                {
+                    for (int i = 0; i < histogram_r.Length; i++)
+                    {
+                        float pct = histogram_r[i] / max;   // What percentage of the max is this value?
+                        g.DrawLine(Pens.Black,
+                            new Point(i, imga.Height - 5),
+                            new Point(i, imga.Height - 5 - (int)(pct * histHeight))  // Use that percentage of the height
+                            );
+                    }
+                }
+                pictureBox4.Image = imga;
+            }
+        }
+    }
+}
